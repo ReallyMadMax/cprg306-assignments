9e6248678685b9ba798555d8f7a79238d557f04d
Max Dilger <ettrevivre@gmail.com>
2024-10-11 15:29:53 -0600

    week 6 content

---
+++ app/week-6/item-list.js
@@ -0,0 +1,53 @@
+"use client"
+import { useState } from "react";
+import Item from "./item";
+export default function ItemList(){
+
+    const items = require('./items.json');
+    
+    const [sortBy, setSortBy] = useState("name")
+
+    const sortedItems = items.sort((a, b) => {
+        if (sortBy === 'name') {
+        return a.name.localeCompare(b.name);
+        } else if (sortBy === 'category') {
+        return a.category.localeCompare(b.category);
+        }
+    });
+    
+    const handleSortByName = () => {setSortBy('name')};
+    
+    const handleSortByCategory = () => {setSortBy('category')};
+
+    let buttonOff = "bg-blue-200 hover:bg-blue-800 rounded text-black mt-5 px-4 py-2";
+    let buttonOn = "bg-blue-500 hover:bg-blue-800 active:bg-yellow-300 rounded text-black mt-5 px-4 py-2";
+
+
+    return (
+        <div style={{justifyContent: "center", alignItems:"center", display: "flex", flexDirection: "column", background: "#999999", padding:"20px", width: "500px", margin: "0 auto"}}>
+            <div style={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+                <button
+                    className={sortBy === 'name' ? buttonOn : buttonOff}
+                    onClick={handleSortByName}
+                    
+                >
+                    Sort by Name
+                </button>
+                <button
+                    className={sortBy === 'category' ? buttonOn : buttonOff}
+                    onClick={handleSortByCategory}
+                >
+                    Sort by Category
+                </button>
+            </div>
+            <ul style={{display: "flex", flexDirection: "column", width: "100%"}}>
+                {sortedItems.map((item) => (
+                <li key={item.id} style={{display: "flex", justifyContent: "space-between", padding: "10px", borderBottom: "1px solid black"}}>
+                    <h2>{item.name}</h2>
+                    <p>{item.category}</p>
+                </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
